688f4d8da49689b6cba1a5e09aa6d39703a753c9
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-17 14:12:48 +0300

    x

---
+++ app/core/db.py
@@ -0,0 +1,34 @@
+import aiosqlite
+
+from .config import Settings
+
+
+class DBConnect:
+    def __init__(self, db_url: str):
+        self.db_url = db_url
+        self.connection: aiosqlite.Connection = None
+        self.cursor: aiosqlite.Cursor = None
+
+    async def connect(self):
+        self.connection = await aiosqlite.connect(self.db_url)
+        self.cursor = await self.connection.cursor()
+
+    async def close(self):
+        await self.cursor.close()
+        await self.connection.close()
+
+    async def execute(self, query: str, params: tuple = ()):
+        await self.cursor.execute(query, params)
+        await self.connection.commit()
+
+    async def fetchall(self, query: str, params: tuple = ()) -> list[tuple]:
+        await self.cursor.execute(query, params)
+        return await self.cursor.fetchall()
+
+    async def fetchone(self, query: str, params: tuple = ()) -> tuple:
+        await self.cursor.execute(query, params)
+        return await self.cursor.fetchone()
+
+
+settings = Settings()
+db = DBConnect(settings.get_db_url())
