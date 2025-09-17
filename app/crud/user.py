e92f5a9f424bf23d8ebdf5d67e6b35af14653f10
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-17 14:45:34 +0300

    p

--- app/crud/user.py
+++ app/crud/user.py
@@ -11,8 +11,8 @@ async def add_user_table(user_id: int):
                        ''')
 
 async def get_user(telegram_id: int):
-    return await db.fetchone('SELECT * FROM users WHERE telegram_id = $1', (telegram_id,))
+    return await db.fetchone('SELECT * FROM customers WHERE telegram_id = $1', (telegram_id,))
 
 
 async def create_user(telegram_id: int, full_name: str, username:str):
-    return await db.execute('INSERT INTO users (telegram_id, full_name, username) VALUES ($1, $2, $3)', (telegram_id, full_name, username,))
+    return await db.execute('INSERT INTO customers (telegram_id, full_name, username) VALUES ($1, $2, $3)', (telegram_id, full_name, username,))
