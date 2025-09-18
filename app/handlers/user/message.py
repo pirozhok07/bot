df587af8f0eeb11775d7deca0c0b5883d48e3040
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-18 10:52:11 +0300

    v

--- app/handlers/user/message.py
+++ app/handlers/user/message.py
@@ -15,9 +15,6 @@ async def start_handler(message: Message):
     await repo.create_user(telegram_id=message.from_user.id, full_name=message.from_user.full_name, username=message.from_user.username)
     await message.answer(f'Hi, {html.escape(message.from_user.full_name)}, добро пожаловать в систему заказов.',
                          reply_markup=kb.start_menu)
-    
-async def show_goods_handler(message: Message):
-    await message.answer('Категории', reply_markup=kb.category_menu)
 
 async def show_category(message: Message):
     await for_db.check_table_products()
@@ -27,5 +24,4 @@ async def show_category(message: Message):
 def register_handlers():
     router.message.register(start_handler, CommandStart())
     # router.message.register(show_product_handler, Command('show_product'))
-    router.message.register(show_goods_handler, F.text == 'Показать товары')
-    router.message.register(show_category, F.text == '/cat')
+    router.message.register(show_category, F.text == 'Показать товары')
