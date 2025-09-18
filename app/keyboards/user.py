2a007b5e0d33422e27290846ba3734e2d7a4ed18
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-18 11:47:23 +0300

    b

--- app/keyboards/user.py
+++ app/keyboards/user.py
@@ -24,7 +24,7 @@ def get_categories_kb(categories):
     category_menu = InlineKeyboardBuilder()
     i=0
     for category in categories:
-        category_menu.add(InlineKeyboardButton(text=category[0], callback_data=f"category_{i}"))
+        category_menu.row(InlineKeyboardButton(text=category[0], callback_data=f"category_{i}"))
         i+=1
     category_menu.add(InlineKeyboardButton(text='назад', callback_data=f"back"))
     return category_menu.as_markup()
@@ -33,7 +33,7 @@ def get_products_kb(products):
     products_menu = InlineKeyboardBuilder()
     i=0
     for product in products:
-        products_menu.add(InlineKeyboardButton(text=f"{product[0]} {product[1]}", callback_data=f"product_{i}"))
+        products_menu.row(InlineKeyboardButton(text=f"{product[0]} {product[1]}", callback_data=f"product_{i}"))
         i+=1
     products_menu.add(InlineKeyboardButton(text='назад', callback_data=f"back"))
     return products_menu.as_markup()
