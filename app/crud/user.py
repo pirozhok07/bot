b86dd3f3af129d5a8230b1be407e1bfd84aa6252
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-18 10:50:42 +0300

    '

--- app/crud/user.py
+++ app/crud/user.py
@@ -3,18 +3,18 @@ from loguru import logger
 
 
 async def get_user(telegram_id: int):
-    a =await db.fetchone('SELECT * FROM customers WHERE telegram_id = $1', (telegram_id,))
+#     a =await db.fetchone('SELECT * FROM customers WHERE telegram_id = $1', (telegram_id,))
     
-    c =await db.fetchone('SELECT * FROM products ', ())
-    logger.error(c)
-    logger.error(a)
-    # return await db.fetchone('SELECT * FROM customers WHERE telegram_id = $1', (telegram_id,))
+#     c =await db.fetchone('SELECT * FROM products ', ())
+#     logger.error(c)
+#     logger.error(a)
+    return await db.fetchone('SELECT * FROM customers WHERE telegram_id = $1', (telegram_id,))
 
 
 async def create_user(telegram_id: int, full_name: str, username:str):
-    logger.error(f"customer {telegram_id} ADD")
-    a =await db.execute('INSERT INTO customers (telegram_id, full_name, username) VALUES ($1, $2, $3)', (telegram_id, full_name, username,))
+    # logger.error(f"customer {telegram_id} ADD")
+    # a =await db.execute('INSERT INTO customers (telegram_id, full_name, username) VALUES ($1, $2, $3)', (telegram_id, full_name, username,))
     
-    logger.error(a)
-    return a
-    # return await db.execute('INSERT INTO customers (telegram_id, full_name, username) VALUES ($1, $2, $3)', (telegram_id, full_name, username,))
+    # logger.error(a)
+    # return a
+    return await db.execute('INSERT INTO customers (telegram_id, full_name, username) VALUES ($1, $2, $3)', (telegram_id, full_name, username,))
