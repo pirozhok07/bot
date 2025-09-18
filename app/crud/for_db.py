8f383c674cbb3fe946fe64f2cf982741a2a05c9e
pirozhok07 <vik.izrantseva@yandex.ru>
2025-09-18 10:21:10 +0300

    g

--- app/crud/for_db.py
+++ app/crud/for_db.py
@@ -76,7 +76,7 @@ async def add_items_into_table_products():
 async def get_categories():
     logger.warning("get_categories")
     a = db.fetchall("SELECT DISTINCT category FROM products WHERE is_available = TRUE")
-    for row in a[0]:
+    for row in a:
         logger.error(a[0])
     logger.error(a)
     logger.warning("get_categories")
